0b268eb0554cf04276230e1f3298ca1ec7c23ab2
otoniricardo <otoniricardo@gmail.com>
2020-03-24 17:06:18 -0300

    Configura reactotron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/reactotron';
+
 import GlobalStyle from './styles/globals';
 import Header from './components/Header';
 import Routes from './routes';
