e53bd78944f18ca383942a206acebe82acade8f2
otoniricardo <otoniricardo@gmail.com>
2020-03-28 09:50:16 -0300

    Adiciona react-toatify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,5 +1,5 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
@@ -18,7 +18,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warn('ERRO');
+    toast.error(`Produto fora do estoque`);
     return;
   }
 
